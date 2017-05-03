014b35e2ff0062a0e3728986706acd75a3853852
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2017-05-02 22:21:49 -0300

    aula 08

--- HandsOn/Aula08/projeto/DeployTool.py
+++ HandsOn/Aula08/projeto/DeployTool.py
@@ -1,5 +1,29 @@
 from Modulos.DockerModulo import DockerModulo
+from Modulos.SSHModulo import SSHModulo
+import json
 
-if __name__ =='main':
-	Docker = DockerModulo()
+if __name__ =='__main__':
+	try:
+		with open ("deployment.json", 'r') as f:
+			job_json = json.loads(f.read())
+	
+		Docker = DockerModulo()
+		#container = Docker.createContainer(job_json['name'])
+		#Docker.startContainer(container)
+
+		ssh = SSHModulo()
+
+
+		for c in job_json['commands']:
+			command = "docker exec %s %s" % (job_json['name'], c)
+
+			ssh.execCommand(c)
+			
+
+
+
+		print 'deploy finalizado com sucesso'
+
+	except Exception as e:
+		print e 
 
